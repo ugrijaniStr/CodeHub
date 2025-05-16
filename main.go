4490987eadbc2af68abdefa5738cb6b2d496292e
ugrijaniStr <gospodaklosari11@gmail.com>
2025-05-16 13:07:50 +0200

    .

--- main.go
+++ main.go
@@ -27,6 +27,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index")
 }
 
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "dashboard")
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if(r.Method == http.MethodPost) {
 		err := r.ParseForm()
@@ -48,18 +52,18 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}).Decode(&user)
 
 		if(err != nil) {
-			http.Error(w, "Korisnik nije pronađen", 401)
+			http.Error(w, "User not found", 401)
 			return
 		}
 
 		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-		
-		if(err != nil) {
-			http.Error(w, "Pogrešna lozinka", 401)
+
+		if err != nil {
+			http.Error(w, "Wrong username or password", 401)
 			return
 		}
 
-		fmt.Fprintln(w, "Uspešna prijava!")
+		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
 		return
 	}
 
@@ -70,7 +74,7 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		err := r.ParseForm()
 		if err != nil {
-			http.Error(w, "Err", 400)
+			http.Error(w, "Error with parsing form.", 400)
 			return
 		}
 
@@ -79,6 +83,24 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		phoneNumber := r.FormValue("phoneNumber")
 		email := r.FormValue("email")
 
+		filter := map[string]interface{}{
+			"$or": []interface{}{
+				map[string]interface{}{"username": username},
+				map[string]interface{}{"email": email},
+			},
+		}
+
+		var existingUser struct {
+			Username string `bson:"username"`
+			Email    string `bson:"email"`
+		}
+
+		err = collection.FindOne(context.TODO(), filter).Decode(&existingUser)
+		if err == nil {
+			http.Error(w, "Username or E-Mail is already exists.", http.StatusConflict)
+			return
+		}
+
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
 			http.Error(w, "Error with hashing password.", 500)
@@ -86,15 +108,18 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		user := map[string]interface{}{
-			"username": username,
-			"password": string(hashedPassword),
-			"email": email,
+			"username":     username,
+			"password":     string(hashedPassword),
+			"email":        email,
 			"phone-number": phoneNumber,
+			"color-name: ": "#000000",
+			"pfp": "",
+			"description": "",
 		}
 
 		_, err = collection.InsertOne(context.TODO(), user)
 		if err != nil {
-			http.Error(w, "Error with register form.", 500)
+			http.Error(w, "Error with register.", 500)
 			return
 		}
 
@@ -110,13 +135,23 @@ func uploadHandler(w http.ResponseWriter, r * http.Request) {
 }
 
 func main() {
-	client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal("Error with mongoDB: ", err)
+	}
+	
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal("Error with connecting on MongoDB: ", err)
+	}
+	
 	collection = client.Database("codehub").Collection("users")
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/dashboard", dashboardHandler)
 
 	port := 8080
 	host := "127.0.0.1"
@@ -125,6 +160,7 @@ func main() {
     [!] http://{{host}}:{{port}}/index
     [!] http://{{host}}:{{port}}/login
     [!] http://{{host}}:{{port}}/register
+    [!] http://{{host}}:{{port}}/dashboard
     [!] http://{{host}}:{{port}}/upload
 	
 	`
